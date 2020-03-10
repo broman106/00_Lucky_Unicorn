2fc9c62cdd5f12db131415ab46967457a177e629
broman106 <60583396+broman106@users.noreply.github.com>
2020-03-10 14:50:37 +1300

    Create 05_End_Game_Mechanics.py

---
+++ 05_End_Game_Mechanics.py
@@ -0,0 +1,46 @@
+# lucky unicorn - payment mechanics
+
+# To do
+# Ask user how much money they have to play with
+#If the total is more than $1, quit
+# If the total is more than $1, ask user if they want to keep going
+
+import random
+
+tokens = ["horse","horse","horse",
+          "zebra", "zebra","zebra",
+          "donkey", "donkey","donkey","unicorn"]
+
+# Assume starting amount is $10
+total = int(input("How much would you like to play with? "))
+
+keep_going = ""
+while keep_going == "":
+
+    # Allow manual token input for testing purposes
+    token = random.choice(tokens)
+
+    # Adjust total correctly for a given token
+    if token == " unicorn":
+        total += 5
+        feedback = "congratulations oyu won $5.00"
+    elif token == "donkey":
+        total -= 1
+        feedback = "sorry, you did not win anything this round"
+    else:
+        total -= 0.5
+        feedback = "congratulations you won 50c"
+
+    print()
+
+    print(feedback)
+    print("you have {} to play with".format(total))
+
+    if total < 1:
+        print("sorry you don't have enough money to continue. game over")
+        keep_going = "end"
+    else:
+        keep_going = input("press <enter> to play again or any key to quit")
+
+print("thank you for playing.")
+
